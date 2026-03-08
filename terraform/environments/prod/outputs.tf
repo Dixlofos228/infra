6c92a589ae2f2db0af7bb2d72a3336fce56279a2
Dixlofos228 <dix.lofos122@gmail.com>
2026-03-08 05:50:06 -0300

    Add monitoring stack: Prometheus, Grafana, Node Exporter and LADR doc

--- terraform/environments/prod/outputs.tf
+++ terraform/environments/prod/outputs.tf
@@ -1,19 +1,9 @@
-output "load_balancer_ip" {
-  description = "IP address of the load balancer"
-  value       = google_compute_global_address.lb_ip.address
+output "monitoring_ip" {
+  description = "Static IP address for monitoring VM"
+  value       = google_compute_address.monitoring.address
 }
 
-output "load_balancer_url" {
-  description = "URL to access the application via load balancer"
-  value       = "http://${google_compute_global_address.lb_ip.address}"
-}
-
-output "vm_public_ip" {
-  description = "Public IP of the VM (for direct access)"
-  value       = google_compute_instance.app_server.network_interface[0].access_config[0].nat_ip
-}
-
-output "vm_private_ip" {
-  description = "Private IP of the VM"
-  value       = google_compute_instance.app_server.network_interface[0].network_ip
+output "monitoring_vm_ip" {
+  description = "Ephemeral IP of the monitoring VM"
+  value       = google_compute_instance.monitoring.network_interface[0].access_config[0].nat_ip
 }
